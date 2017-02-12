89450b3989156b989126a4b0b6e489415438208e
p <koldfuzor@gmail.com>
2017-02-12 13:35:42 -0800

    test optional disabling of Crashlytics

--- lterm-android/jni/Android.mk
+++ lterm-android/jni/Android.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
+include $(LOCAL_PATH)/LFL_BIN_DIR.mk
 include $(CLEAR_VARS)
 LOCAL_MODULE := app
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../android/term/libapp.so
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../$(LFL_BIN_DIR)/term/libapp.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 #include $(call all-subdir-makefiles)
--- lterm-android/src/MainActivity.java
+++ lterm-android/src/MainActivity.java
@@ -11,9 +11,16 @@ public class MainActivity extends com.lucidfusionlabs.app.MainActivity {
     }
 
     @Override protected void onCreated() {
-        if (!BuildConfig.DEBUG) {
-            Log.i("lfl", "Initializing Crashlytics");
-            Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
+        if (preferences.getString("send_crash_reports", "1").equals("1")) {
+            if (BuildConfig.DEBUG) Log.i("lfl", "BuildConfig.DEBUG skipping Crashlytics");
+            else {
+                Log.i("lfl", "Initializing Crashlytics");
+                Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
+                String crash_report_name  = preferences.getString("crash_report_name",  "");
+                String crash_report_email = preferences.getString("crash_report_email", "");
+                if (crash_report_name .length() > 0) Crashlytics.setUserName (crash_report_name);
+                if (crash_report_email.length() > 0) Crashlytics.setUserEmail(crash_report_email);
+            }
         }
     }
 }
