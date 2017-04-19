cba765cc3829e06d26083ba6a909004e4603bf8d
p <koldfuzor@gmail.com>
2017-04-19 14:33:13 -0700

    hide keyboard toolbar when tab loses focus

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 * **[LTerminal-ios](https://github.com/LucidFusionLabs/LTerminal-ios)**:         [![Build Status](https://travis-ci.org/LucidFusionLabs/LTerminal-ios.svg?branch=master)](https://travis-ci.org/LucidFusionLabs/LTerminal-ios)
 * **[LTerminal-android](https://github.com/LucidFusionLabs/LTerminal-android)**: [![Build Status](https://travis-ci.org/LucidFusionLabs/LTerminal-android.svg?branch=master)](https://travis-ci.org/LucidFusionLabs/LTerminal-android)
 
-See external [project page](http://lucidfusionlabs.com/terminal)
+See external [project page](http://www.lucidfusionlabs.com/LTerminal)
--- term.h
+++ term.h
@@ -41,11 +41,20 @@ struct TerminalTabInterface : public Dialog {
   virtual KeyboardController *GetKeyboardTarget() = 0;
   virtual Box GetLastDrawBox() = 0;
   virtual void DrawBox(GraphicsDevice*, Box draw_box, bool check_resized) = 0;
-  virtual void LoseFocus() { root->active_textbox = root->default_textbox(); root->active_controller = root->default_controller(); }
-  virtual void TakeFocus() { root->active_textbox = GetKeyboardTarget();     root->active_controller = GetMouseTarget();
+
+  virtual void LoseFocus() {
+    root->active_textbox = root->default_textbox();
+    root->active_controller = root->default_controller();
+    if (toolbar) toolbar->Show(false);
+  }
+
+  virtual void TakeFocus() {
+    root->active_textbox = GetKeyboardTarget();
+    root->active_controller = GetMouseTarget();
     UpdateControllerWait();
     if (toolbar) toolbar->Show(true);
   }
+
   virtual void UpdateControllerWait() {
     if (controller && controller->frame_on_keyboard_input) app->scheduler.AddMainWaitKeyboard(root);
     else                                                   app->scheduler.DelMainWaitKeyboard(root);
